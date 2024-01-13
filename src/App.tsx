9ebd1ff815d474090c2efe5905e4fd47fa1480e2
aaaa1597 <tokushi2011@gmail.com>
2024-01-13 16:04:20 +0900

    ベースコミット2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { helloif } from './asm-cpp/helloif';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       hello world!!
-      <button onClick={() => helloif(11, 22, setCount)}> Click {count} times</button>
-      <div>count= {count}</div>
-  </div>
+    </div>
   );
 }
 
