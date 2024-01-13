cdee69f9f5217b1606fcc0ed9be25bcdd5aaf229
aaaa1597 <tokushi2011@gmail.com>
2024-01-13 16:38:13 +0900

    完成。App.tsxからWebAssemblyを呼出すことが出来た。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const getFromAsm = () => {
+    WebAssembly.instantiateStreaming(fetch("wasm/hello.wasm")).then(
+      (obj) => {
+        console.log('----------001')
+        const ret1 = (obj.instance.exports._Z3addii as CallableFunction)(11, 22);
+        console.log('retretret=', ret1);
+        setCount(ret1)
+        const ret2 = (obj.instance.exports._Z3subii as CallableFunction)(11, 22);
+        console.log('retretret=', ret2);
+        console.log('----------002')
+        return ret1
+      }
+    );
+  }
+  
   return (
     <div className="App">
       hello world!!
-    </div>
+      <button onClick={() => getFromAsm()}> Click {count} times</button>
+  </div>
   );
 }
 
